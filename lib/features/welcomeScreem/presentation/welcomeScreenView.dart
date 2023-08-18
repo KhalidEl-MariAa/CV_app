e2ddc96993769b671c356d4daf2a13090685c524
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-18 11:32:31 -0700

    responsive dimensions, update ui of welcomescreen

--- lib/features/welcomeScreem/presentation/welcomeScreenView.dart
+++ lib/features/welcomeScreem/presentation/welcomeScreenView.dart
@@ -11,36 +11,33 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-  List<PageViewModel> pages= [
-    PageViewModel(
-      body: 'Time To have CV',
-      title: 'Welcome...',
-      image: const Image(image: AssetImage('lib/core/assets/images/hello.jpg')),
-      decoration: PageDecoration(
-
-        titleTextStyle: titlefont.copyWith(fontSize: 30,letterSpacing: 3,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
-        bodyTextStyle: bodyfont) 
-        
-      
-
-    ),
-     PageViewModel(
-      body: 'No problem, with us you can create it easily and free...so you can apply to the job you want ,Only follow the flow',
-      title: "doesn't  have exprience to create your CV?",
-      image: const Image(image: AssetImage('lib/core/assets/images/cv.jpg')),
-      decoration: PageDecoration(
-        titleTextStyle: titlefont.copyWith(fontSize: 30,letterSpacing: 3,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
-        bodyTextStyle: bodyfont )
-
-      
-      
-
-    )
-  ];
+  
 
   
   @override
   Widget build(BuildContext context) {
+    
+    List<PageViewModel> pages= [
+    PageViewModel(
+      body: "Haven't a CV yet?",
+      title: 'Welcome...',
+      image: const Image(image: AssetImage('lib/core/assets/images/hello.jpg')),
+      decoration: PageDecoration(
+        bodyPadding: const EdgeInsets.only(top: 17),
+        titleTextStyle: titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.08,letterSpacing: 3,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+        bodyTextStyle: bodyfont.copyWith(fontSize: MediaQuery.of(context).size.width*0.04,letterSpacing: 5.25)) 
+    ),
+     PageViewModel(
+      body: 'No problem, with us you can create it easily and free...so you can apply to the job you want ,Only follow the flow',
+      title: "Doesn't  have exprience to create your CV?",
+      image: const Image(image: AssetImage('lib/core/assets/images/cv.jpg')),
+      decoration: PageDecoration(
+        bodyPadding:const EdgeInsets.only(top: 25),
+        titleTextStyle: titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.07,letterSpacing: 3,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+        bodyTextStyle: bodyfont.copyWith(fontSize: MediaQuery.of(context).size.width*0.045) )
+    )
+  ];
+
     return SafeArea(
       child: Scaffold(
         body: Center(
@@ -50,8 +47,16 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             globalBackgroundColor: Colors.white,
           pages: pages,
           onDone: (){},
-          done: ElevatedButton(child: const Text('Done'),onPressed: (){},),
+          done: ElevatedButton(
+            onPressed: (){},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.black,
+              textStyle: bodyfont.copyWith(),
+            ),
+            child: const Text('Done')
+            ,),
           showNextButton: false,
+          
           bodyPadding: const EdgeInsets.symmetric(horizontal: 20),
           
         
