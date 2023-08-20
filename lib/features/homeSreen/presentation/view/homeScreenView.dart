4c311d9b745d077590f28e8ad3e80a4313f5ef1a
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-20 08:15:20 -0700

    add google nav bar package,use it

--- lib/features/homeSreen/presentation/view/homeScreenView.dart
+++ lib/features/homeSreen/presentation/view/homeScreenView.dart
@@ -1,5 +1,6 @@
+import 'package:cv_app/core/utils/fonts.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_nav_bar/google_nav_bar.dart';
 class HomeScreenView extends StatefulWidget {
   const HomeScreenView({super.key});
 
@@ -10,8 +11,32 @@ class HomeScreenView extends StatefulWidget {
 class _HomeScreenViewState extends State<HomeScreenView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return SafeArea(
+      child: Scaffold(
+        
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+           gradient: const LinearGradient(
+              colors: [Colors.purple,Colors.blue],
+              
+            ),
+            borderRadius: BorderRadius.circular(20)
+          ),
+          child: GNav(
+            
+            iconSize: 25,
+             gap: 5,
+             textStyle: titlefont.copyWith(color: Colors.amber),
+            activeColor: Colors.black,
+            tabs:const [
+              GButton(icon: Icons.lightbulb,iconColor: Colors.amber,text: 'Instructions', ),
+               GButton(icon: Icons.call,iconColor: Colors.white,text: 'Contact us',),
+               GButton(icon: Icons.border_color_outlined,iconColor: Colors.white,text: 'Create Resume',),
+
+                
+            ] ),
+        ) ,
+      ),
     );
   }
 }
