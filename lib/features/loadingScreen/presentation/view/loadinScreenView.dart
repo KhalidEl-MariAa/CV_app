fbc535a3c7df4ced64bf03790a118f27d8ba27ba
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-20 05:48:55 -0700

    Loading ScreenUi done :)

--- lib/features/loadingScreen/presentation/view/loadinScreenView.dart
+++ lib/features/loadingScreen/presentation/view/loadinScreenView.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 
-class LoadingScreenView extends StatelessWidget {
+class LoadingScreenView extends StatefulWidget {
   const LoadingScreenView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
+  State<LoadingScreenView> createState() => _LoadingScreenViewState();
+}
 
-      
+
+class _LoadingScreenViewState extends State<LoadingScreenView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    
+  }
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    
+    return  SafeArea(
+      child: Scaffold(
+    
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              LoadingAnimationWidget.flickr(
+                leftDotColor: Colors.red,
+                 rightDotColor: Colors.black,
+                  size: MediaQuery.of(context).size.width*0.11,
+                  )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
