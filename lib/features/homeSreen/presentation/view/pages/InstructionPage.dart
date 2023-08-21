05a3938ebf24ca5f2ca872b32726fa0ad444455c
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-21 09:56:45 -0700

    factorize our code

--- lib/features/homeSreen/presentation/view/pages/InstructionPage.dart
+++ lib/features/homeSreen/presentation/view/pages/InstructionPage.dart
@@ -1,5 +1,5 @@
+import 'package:cv_app/features/homeSreen/presentation/view/widgets/instructionsSecondHalf.dart';
 import 'package:flutter/material.dart';
-import '../../../../../core/utils/fonts.dart';
 import '../widgets/instructionsFirstHalf.dart';
 
 
@@ -21,102 +21,7 @@ class _InstructionsViewState extends State<InstructionsView> {
         flexibleSpace:const InstructionsViewFirstHalf()
       )];
     }, 
-    body: Container(decoration: const BoxDecoration(
-      gradient: LinearGradient(colors: [Colors.blueGrey,Colors.grey])
-    ),
-    child: ListView(
-         physics: const NeverScrollableScrollPhysics(),
-      children:[ Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:8.0,vertical: 10),
-                    child: Text('Instructions to have a high ATP score ',
-                    style:titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.05,fontWeight: FontWeight.bold) ,
-                    ),
-                  ),
-                  
-                  Padding(
-                    padding: const EdgeInsets.only(left:8.0,right: 8,bottom: 12),
-                    child: Text('- As you know having high score meaning you are qualified for interview',
-                    style:titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.04,fontWeight: FontWeight.bold) ,
-                    ),
-                  ),
-                  Image.asset('lib/core/assets/images/interview.png',width:MediaQuery.of(context).size.width*0.7 ,),
-                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:8.0,vertical: 15),
-                    child: Text('To achieve interview please follow these instructions : ',
-                    style:titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.05,fontWeight: FontWeight.bold) ,
-                    ),
-                  ),
-                    Padding(
-                    padding: const EdgeInsets.only(left:8.0,right: 8,bottom: 12),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text('- Read the job description very well .',
-                      style:titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.04,fontWeight: FontWeight.bold) ,
-                      ),
-                    ),
-                  ),
-                    Padding(
-                    padding: const EdgeInsets.only(left:8.0,right: 8,bottom: 12),
-                    child: Text('- Make sure you understand the requirements',
-                    style:titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.04,fontWeight: FontWeight.bold) ,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left:8.0,right: 8,bottom: 12),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text('- Analyze skills you need for job .',
-                      style:titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.04,fontWeight: FontWeight.bold) ,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left:8.0,right: 8,bottom: 12),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text('- After analyzing , try to write in your resume words from the job advertisment .',
-                      style:titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.04,fontWeight: FontWeight.bold) ,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left:8.0,right: 8,bottom: 12),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text('- Write the skills as it required or very close to them .',
-                      style:titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.04,fontWeight: FontWeight.bold) ,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left:8.0,right: 8,bottom: 12),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text('- Write strong words about communication skills and self confidence as (iam a leader - can work with team - my team as family - love working under pressure) .',
-                      style:titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.04,fontWeight: FontWeight.bold) ,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left:8.0,right: 8,bottom: 30),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text('- Avoid lying about yourself .',
-                      style:titlefont.copyWith(fontSize: MediaQuery.of(context).size.width*0.04,fontWeight: FontWeight.bold) ,
-                      ),
-                    ),
-                  ),
-                  
-                  
-
-        ],
-      ),
-    ]),
-    )
+    body: const InstructionsViewSecondHalf()
     ) ;
   }
 }
