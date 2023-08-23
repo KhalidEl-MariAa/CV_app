475b94507a05afe1f51c2a339997e55572475c80
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-23 07:34:38 -0700

    contact us view ui done

--- lib/features/homeSreen/presentation/view/pages/contactUspage.dart
+++ lib/features/homeSreen/presentation/view/pages/contactUspage.dart
@@ -1,3 +1,4 @@
+import 'package:cv_app/core/utils/fonts.dart';
 import 'package:flutter/material.dart';
 
 class ContactUsView extends StatelessWidget {
@@ -5,6 +6,28 @@ class ContactUsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  
+       Center(
+         child: CustomScrollView(
+           slivers:[ SliverFillRemaining(
+            hasScrollBody: false,
+             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('We promise we will review it.. ',
+              style: titlefont.copyWith(fontWeight: FontWeight.bold,fontSize: MediaQuery.of(context).size.width*0.05)),
+              const SizedBox(height: 25,),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black
+                ),
+                onPressed: (){},
+               child: Text('Contact Us',style: bodyfont.copyWith(color: Colors.white),)) ],
+              ),
+           ),]
+         ),
+       );
+      
+    
   }
 }
