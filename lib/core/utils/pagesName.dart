293a4bc63dd280554c8b23d60ceec3c3262e079c
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-25 16:57:26 -0700

    refactor languages choices

--- lib/core/utils/pagesName.dart
+++ lib/core/utils/pagesName.dart
@@ -1,11 +1,51 @@
 
 
 
+import 'package:multi_select_flutter/util/multi_select_item.dart';
+
 abstract class PagesNames{
 static const welcomeScreenName= '/';
 static const loadingScreenName='LoadingScreen';
 static const homeScreenName='HomeScreen';
 static const contactUsDetails='ContactUsDetailsScreen';
+static List<MultiSelectItem<dynamic>> languages= [
+                      MultiSelectItem('Arabic','Arabic'),
+                      MultiSelectItem('English A1','English A1'),
+                      MultiSelectItem('English A2','English A2'),
+                      MultiSelectItem('English A3','English A3'),
+                      MultiSelectItem('English B1','English B1'),
+                      MultiSelectItem('English B2','English B2'),
+                      MultiSelectItem('English B3','English B3'),
+                      MultiSelectItem('English C1','English C1'),
+                      MultiSelectItem('English C2','English C2'),
+                      MultiSelectItem('English C3','English C3'),
+                      MultiSelectItem('German A1','German A1'),
+                      MultiSelectItem('German A2','German A2'),
+                      MultiSelectItem('German B1','German B1'),
+                      MultiSelectItem('German B2','German B2'),
+                      MultiSelectItem('German C1','German C1'),
+                      MultiSelectItem('German C2','German C2'),
+                      MultiSelectItem('French A1','French A1'),
+                      MultiSelectItem('French A2','French A2'),
+                      MultiSelectItem('French B1','French B1'),
+                      MultiSelectItem('French B2','French B2'),
+                      MultiSelectItem('French C1','French C1'),
+                      MultiSelectItem('French C2','French C2'),
+                      MultiSelectItem('Italian A1','Italian A1'),
+                      MultiSelectItem('Italian A2','Italian A2'),
+                      MultiSelectItem('Italian B1','Italian B1'),
+                      MultiSelectItem('Italian B2','Italian B2'),
+                      MultiSelectItem('Italian C1','Italian C1'),
+                      MultiSelectItem('Italian C2','Italian C2'),
+                      MultiSelectItem('Spanish A1','Spanish A1'),
+                      MultiSelectItem('Spanish A2','Spanish A2'),
+                      MultiSelectItem('Spanish B1','Spanish B1'),
+                      MultiSelectItem('Spanish B2','Spanish B2'),
+                      MultiSelectItem('Spanish C1','Spanish C1'),
+                      MultiSelectItem('Spanish C2','Spanish C2'),
+                      
+
+                      ];
 }
 
 
