dc0ab0f297981e90bfe1d33b101af7c17537b041
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-25 17:02:08 -0700

    refactor the code , remove unused imports

--- lib/features/homeSreen/presentation/view/pages/createResume.dart
+++ lib/features/homeSreen/presentation/view/pages/createResume.dart
@@ -1,8 +1,6 @@
-import 'package:cv_app/core/utils/fonts.dart';
-import 'package:cv_app/core/utils/pagesName.dart';
+import 'package:cv_app/features/homeSreen/presentation/view/widgets/createResumePageWidgets/chooseLanguage.dart';
 import 'package:cv_app/features/homeSreen/presentation/view/widgets/createResumePageWidgets/personalDetails.dart';
 import 'package:flutter/material.dart';
-import 'package:multi_select_flutter/multi_select_flutter.dart';
 
 class CreateResumeView extends StatefulWidget {
   const CreateResumeView({super.key});
@@ -39,37 +37,7 @@ class _CreateResumeViewState extends State<CreateResumeView> {
                     const SizedBox(height: 20,),
                     Divider(color: Colors.black.withOpacity(0.5),thickness: 0.7,),
                    const SizedBox(height: 20,),
-                    Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                   Icon(Icons.language_rounded,size: 50,color: Colors.black.withOpacity(0.64),),const SizedBox(width: 10,)
-                  ,Text('Languages :',style: titlefont.copyWith(fontSize: 20,textBaseline: TextBaseline.alphabetic,fontWeight: FontWeight.bold),)
-                  ],
-                  ),
-                    
-                     Padding(
-                       padding: const EdgeInsets.symmetric(vertical:20 , horizontal:13),
-                       child: MultiSelectDialogField(
-                        chipDisplay: MultiSelectChipDisplay(
-                        textStyle: bodyfont),
-                        backgroundColor: Colors.white,
-                        buttonText:Text('Choose your languages',style: bodyfont, ),
-                        validator: (value) {
-                        if(value!.isEmpty){
-                          return 'Please choose language';
-                        }
-                        return null;
-                        },
-                        decoration: BoxDecoration(
-                     
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(35)
-                        ),
-                        items: PagesNames.languages,
-                         onConfirm: (p0) {
-                          
-                        },),
-                     )
+                   const ChooseLanguagesView()
                            
                   ],
                 ),]
---
+++ lib/features/homeSreen/presentation/view/widgets/createResumePageWidgets/chooseLanguage.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:multi_select_flutter/chip_display/multi_select_chip_display.dart';
+import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
+
+import '../../../../../../core/utils/fonts.dart';
+import '../../../../../../core/utils/pagesName.dart';
+
+class ChooseLanguagesView extends StatelessWidget {
+  const ChooseLanguagesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [ Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                   Icon(Icons.language_rounded,size: 50,color: Colors.black.withOpacity(0.64),),const SizedBox(width: 10,)
+                  ,Text('Languages :',style: titlefont.copyWith(fontSize: 20,textBaseline: TextBaseline.alphabetic,fontWeight: FontWeight.bold),)
+                  ],
+                  ),
+                    
+                     Padding(
+                       padding: const EdgeInsets.symmetric(vertical:20 , horizontal:13),
+                       child: MultiSelectDialogField(
+                        chipDisplay: MultiSelectChipDisplay(
+                        textStyle: bodyfont),
+                        backgroundColor: Colors.white,
+                        buttonText:Text('Choose your languages',style: bodyfont, ),
+                        validator: (value) {
+                        if(value!.isEmpty){
+                          return 'Please choose language';
+                        }
+                        return null;
+                        },
+                        decoration: BoxDecoration(
+                     
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(35)
+                        ),
+                        items: PagesNames.languages,
+                         onConfirm: (p0) {
+                          
+                        },),
+                     )],
+    );
+  }
+}
