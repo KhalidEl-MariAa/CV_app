18d162541d74e1c1bfb3cc9d3ce3eb955c8e7a62
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-27 05:25:33 -0700

    change to boxadapter and build skills field

--- lib/features/homeSreen/presentation/view/pages/createResume.dart
+++ lib/features/homeSreen/presentation/view/pages/createResume.dart
@@ -1,6 +1,7 @@
 import 'package:cv_app/features/homeSreen/presentation/view/widgets/createResumePageWidgets/chooseLanguage.dart';
 import 'package:cv_app/features/homeSreen/presentation/view/widgets/createResumePageWidgets/personalDetails.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/constants/const.dart';
 
 class CreateResumeView extends StatefulWidget {
   const CreateResumeView({super.key});
@@ -19,19 +20,56 @@ class _CreateResumeViewState extends State<CreateResumeView> {
         color: Colors.white,
         child: CustomScrollView(
            
-              slivers:[SliverFillRemaining(
-                hasScrollBody: false,
-                child: Column(
-                  children: [
-                    const PersonalDetailsView(),
+               slivers:[
+                SliverToBoxAdapter(
+                 child: Column(
+                 
+                   children: [
+                     const PersonalDetailsView(),
+                     const SizedBox(height: 20,),
+                     Divider(color: Colors.black.withOpacity(0.5),thickness: 0.7,),
                     const SizedBox(height: 20,),
-                    Divider(color: Colors.black.withOpacity(0.5),thickness: 0.7,),
-                   const SizedBox(height: 20,),
-                   const ChooseLanguagesView(),
-                   
-                           ],
+                    const ChooseLanguagesView(),
+                     Divider(color: Colors.black.withOpacity(0.5),thickness: 0.7,),
+                     const SizedBox(height: 20,)
+                     
+                            ],
+                 ),
+               ),
+               SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom:20),
+                  child: ListView.separated(
+                    padding: const EdgeInsets.symmetric(horizontal: 13),
+                    separatorBuilder: (context, index) => const SizedBox(height: 20,),
+                         shrinkWrap: true,
+                         physics: const NeverScrollableScrollPhysics(),
+                         itemCount: 1,
+                         itemBuilder: (context, index) => TextFormField(
+                         validator: (value) {
+                           if(value!.isEmpty){
+                             return "Can't be empty";
+                           }
+                           return null;
+                         },
+                         style:bodyfont ,
+                         decoration: InputDecoration(
+                           border: OutlineInputBorder(borderRadius: BorderRadius.circular(35)),
+                           hintText: 'Skill',
+                           hintStyle: bodyfont,
+                           suffixIcon: GestureDetector(
+                            onTap: (){
+                             
+                            },
+                            child: const Icon(Icons.add,size: 30,))
+                           
+                           
+                         ),
+                       ),),
                 ),
-              ),]
+                    
+                    
+               )]
         ),
       ),
     );
