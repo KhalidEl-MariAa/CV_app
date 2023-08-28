accfa3abd9fcf3cbb688a3ff35e72b097893c7ed
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-28 05:11:06 -0700

    Skills UI done

--- lib/features/homeSreen/presentation/view/widgets/createResumePageWidgets/addSkills.dart
+++ lib/features/homeSreen/presentation/view/widgets/createResumePageWidgets/addSkills.dart
@@ -15,48 +15,63 @@ class _AddSkillsViewState extends State<AddSkillsView> {
   Widget build(BuildContext context) {
     return Padding(
                   padding: const EdgeInsets.only(bottom:20),
-                  child: ListView.separated(
-                    padding: const EdgeInsets.symmetric(horizontal: 13),
-                    separatorBuilder: (context, index) => const SizedBox(height: 20,),
-                         shrinkWrap: true,
-                         physics: const NeverScrollableScrollPhysics(),
-                         itemCount: skillNum,
-                         itemBuilder: (context, index) => CustomTextField(
-                          validateString:"Can't be empty" ,
-                           hintText: 'skills', 
-                           hintstyle: bodyfont,
-                           suffixIcon: skillNum==1? 
-                            GestureDetector(
-                            child: const Icon(Icons.add,size: 30,),
-                            onTap: (){
-                             setState(() {
-                               skillNum++;
-                             });
-                            },
-                            )
-                            :
-                             Row(
-                              
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                               GestureDetector(
-                            child: const Icon(Icons.disabled_by_default_outlined,size: 28,),
-                             onTap: (){
-                             setState(() {
-                               skillNum--;
-                             });
-                            },),
+                  child: Column(
+                    children: [
+                       
+        Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                   Icon(Icons.auto_graph_outlined,size: 50,color: Colors.black.withOpacity(0.64),),const SizedBox(width: 10,)
+                  ,Text('Skills:',style: titlefont.copyWith(fontSize: 20,textBaseline: TextBaseline.alphabetic,fontWeight: FontWeight.bold),)
+                  ],
+                  ),
+                 const SizedBox(height: 20,),
+                      ListView.separated(
+                        padding: const EdgeInsets.symmetric(horizontal: 13),
+                        separatorBuilder: (context, index) => const SizedBox(height: 20,),
+                             shrinkWrap: true,
+                             physics: const NeverScrollableScrollPhysics(),
+                             itemCount: skillNum,
+                             itemBuilder: (context, index) => CustomTextField(
+                              validateString:"Can't be empty" ,
+                               hintText: 'skills', 
+                               hintstyle: bodyfont,
+                               suffixIcon: skillNum==1? 
+                                GestureDetector(
+                                child: const Icon(Icons.add,size: 30,),
+                                onTap: (){
+                                 setState(() {
+                                   skillNum++;
+                                   
+                                 });
+                                 
+                                },
+                                )
+                                :
+                                 Row(
+                                  
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                   GestureDetector(
+                                child: const Icon(Icons.delete,size: 28,),
+                                 onTap: (){
+                                 setState(() {
+                                   skillNum--;
+                                 });
+                                },),
 
-                             GestureDetector(
-                            child: const Icon(Icons.add,size: 30,),
-                            onTap: (){
-                             setState(() {
-                               skillNum++;
-                             });
-                            },
-                            ),])
+                                 GestureDetector(
+                                child: const Icon(Icons.add,size: 30,),
+                                onTap: (){
+                                 setState(() {
+                                   skillNum++;
+                                 });
+                                },
+                                ),])
     )
-    )
+    ),
+                    ],
+                  )
     );
     
                 
