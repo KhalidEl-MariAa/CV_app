117c6af5be1d47c0ab9c00a84df0d793347862bd
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-08-31 10:39:28 -0700

    add new fields to the model

--- lib/features/homeSreen/data/models/resume_Model.dart
+++ lib/features/homeSreen/data/models/resume_Model.dart
@@ -2,31 +2,43 @@ import 'dart:io';
 
 class ResumeModel{
 
+String? headline;
 String? fullname;
 String? phoneNumber;
 String? email;
 String? address;
 String? city;
 String? learningDegree;
+String? summary;
+String? college;
+String? university;
+List? courses;
 List? languages;
 List? skills;
-List? courses;
+List? workexp;
+List? projects;
 File? imageFile;
 
 ResumeModel(
   {
+    this.headline,
      this.fullname,
      this.phoneNumber,
      this.email,
      this.address,
      this.city,
      this.learningDegree,
+     this.summary,
+     this.college,
+     this.university,
+     this.projects,
      this.languages,
      this.skills,
      this.courses,
-     this.imageFile,});
+     this.imageFile,
+     this.workexp});
 
-    factory ResumeModel.FromFields(){
+    factory ResumeModel.fromFields(){
       return ResumeModel();
     }
 
