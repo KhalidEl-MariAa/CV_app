ff2c293c14d2ec12290285bb4b95f8e9952fcdbf
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-09-02 09:53:59 -0700

    remove unused factory constructor

--- lib/features/homeSreen/data/models/resume_Model.dart
+++ lib/features/homeSreen/data/models/resume_Model.dart
@@ -40,25 +40,22 @@ File? imageFile;
 
 ResumeModel(
   {
-    this.headline,
-     this.fullname,
-     this.phoneNumber,
-     this.email,
-     this.address,
-     this.city,
-     this.learningDegree,
-     this.summary,
-     this.college,
-     this.university,
-     this.projects,
-     this.languages,
-     this.skills,
-     this.courses,
-     this.imageFile,
-     this.workexp});
+     this.headline,
+      this.fullname,
+      this.phoneNumber,
+      this.email,
+      this.address,
+      this.city,
+      this.learningDegree,
+      this.summary,
+      this.college,
+      this.university,
+      this.projects,
+      this.languages,
+      this.skills,
+      this.courses,
+      this.imageFile,
+      this.workexp});
 
-    factory ResumeModel.fromFields(){
-      return ResumeModel();
-    }
 
 }
