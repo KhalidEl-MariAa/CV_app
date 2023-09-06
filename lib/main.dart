a148bd6a6b5546034779bcf91ef92b40635a6ad3
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-09-06 04:52:46 -0700

    oprn hive box

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cv_app/core/utils/constants/const.dart';
 import 'package:cv_app/core/utils/pagesName.dart';
 import 'package:cv_app/features/contactUsDetailsScreen/presentation/view/contactUsDetailsScreen.dart';
 import 'package:cv_app/features/homeSreen/data/models/resume_Model.dart';
@@ -11,6 +12,7 @@ import 'package:hive_flutter/adapters.dart';
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(ResumeModelAdapter());
+  await Hive.openBox<ResumeModel>(kBoxName);
   runApp(const MyApp());
 }
 
