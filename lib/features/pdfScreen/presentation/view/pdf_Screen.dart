c3041641018980917e3e5bd487a2dfc8837c05a8
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-09-18 18:53:25 -0700

    clear box after dispose the pdf view

--- lib/features/pdfScreen/presentation/view/pdf_Screen.dart
+++ lib/features/pdfScreen/presentation/view/pdf_Screen.dart
@@ -1,11 +1,32 @@
 import 'dart:io';
 
+import 'package:cv_app/features/homeSreen/data/models/resume_Model.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class PDFVieew extends StatelessWidget {
+import '../../../../core/utils/constants/const.dart';
+
+class PDFVieew extends StatefulWidget {
   const PDFVieew({super.key});
 
+  @override
+  State<PDFVieew> createState() => _PDFVieewState();
+}
+
+class _PDFVieewState extends State<PDFVieew> {
+  
+void delData()async {
+  var box = Hive.box<ResumeModel>(kBoxName);
+  await box.clear();
+}
+
+ @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    delData();
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
